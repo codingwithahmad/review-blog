f4bf175a93944292cc71399afea49faf1ab6691e
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-08-12 22:08:24 +0430

    2-commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
 def index(request):
 	return HttpResponse("Hello")
+
+def api(request):
+	data = {
+		"1": {
+		"title": "مقاله اول",
+		"id": 20,
+		"slug": "first_article",
+		},
+		"2": {
+		"title": "مقاله اول",
+		"id": 20,
+		"slug": "second_article",
+		}
+		,"3": {
+		"title": "مقاله اول",
+		"id": 20,
+		"slug": "third_article",
+		}
+		,"4": {
+		"title": "مقاله اول",
+		"id": 20,
+		"slug": "fourth_article",
+		},
+	}
+	return JsonResponse(data)
