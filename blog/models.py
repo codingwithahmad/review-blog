ea9c19061e7e1a271f5f7d6c104929434c5d0399
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-08-14 20:25:36 +0430

    Add Article model to blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Article(models.Model):
+	title = models.CharField(max_length=200)
+	slug = models.SlugField(max_length=100, unique=True)
+	description = models.TextField()
+	thumbnail = models.ImageField(upload_to="images")
+	publish = models.DateTimeField(default=timezone.now)
