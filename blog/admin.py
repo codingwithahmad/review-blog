58a8a27e96cd2ae3209031936f111a54d1334de1
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-08-17 21:37:18 +0430

    commit-08

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Article
 # Register your models here.
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+	list_display = ("title", "slug", "publish", "status",)
+	list_filter = ("publish", "status")
+	search_fields = ('title', 'slug')
+	prepopulated_fields = {"slug": ("title",)}
+	ordering = ['-status', '-publish']
+
+
+
