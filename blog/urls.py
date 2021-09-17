df676ede7d149d9b9a9af0cb7bc64ce6aed9eeee
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-09-17 19:53:32 +0430

    commit-30

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name="blog"
 urlpatterns = [
-	path('', views.index, name="index"),
-	path('page/<int:page>', views.index, name="index"),
+	path('', views.ArticlesList.as_view(), name="index"),
+	path('page/<int:page>', views.ArticlesList.as_view(), name="index"),
 	path('detail/<slug:slug>', views.detail, name="detail"),
 	path('category/<slug:slug>', views.category, name="category"),
 	path('category/<slug:slug>/page/<int:page>', views.category, name="category")
--- blog/views.py
+++ blog/views.py
@@ -2,18 +2,25 @@ from django.shortcuts import render, get_object_or_404
 #from django.http import HttpResponse, JsonResponse
 from .models import Article, Category
 from django.core.paginator import Paginator
+from django.views.generic.list import ListView
 # Create your views here.
 
-def index(request, page=1):
-	articles_list = Article.objects.published()
-	pagiator = Paginator(articles_list, 1)
-	articles = pagiator.get_page(page)
-	context = {
-		"articles": articles,
-	}
+# def index(request, page=1):
+# 	articles_list = Article.objects.published()
+# 	pagiator = Paginator(articles_list, 1)
+# 	articles = pagiator.get_page(page)
+# 	context = {
+# 		"articles": articles,
+# 	}
 
-	return render(request, 'blog/home.html', context)
+# 	return render(request, 'blog/home.html', context)
 
+
+class ArticlesList(ListView):
+	# model = Article
+	template_name = "blog/home.html"
+	queryset = Article.objects.published()
+	paginate_by = 1
 
 def detail(request, slug):
 	context = {
