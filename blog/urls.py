83e24bf5cbc3427853081d69668c629cd4c45106
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-09-19 19:58:01 +0430

    commit-32

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 	path('', views.ArticlesList.as_view(), name="index"),
 	path('page/<int:page>', views.ArticlesList.as_view(), name="index"),
 	path('detail/<slug:slug>', views.ArticleDetail.as_view(), name="detail"),
-	path('category/<slug:slug>', views.category, name="category"),
-	path('category/<slug:slug>/page/<int:page>', views.category, name="category")
+	path('category/<slug:slug>', views.CategoryList.as_view(), name="category"),
+	path('category/<slug:slug>/page/<int:page>', views.CategoryList.as_view(), name="category")
 ]
--- blog/views.py
+++ blog/views.py
@@ -18,7 +18,6 @@ from django.views.generic import ListView, DetailView
 
 class ArticlesList(ListView):
 	# model = Article
-	template_name = "blog/home.html"
 	queryset = Article.objects.published()
 	paginate_by = 1
 
@@ -35,14 +34,30 @@ class ArticleDetail(DetailView):
 		return get_object_or_404(Article.objects.published(), slug=slug)
 
 
-def category(request, slug, page=1):
-	category = get_object_or_404(Category, slug=slug, status=True)
-	articles_list = category.articles.published()
-	pagiator = Paginator(articles_list, 1)
-	articles = pagiator.get_page(page)
-	context = {
-		"articles": articles,
-		"category": category
-	}
+# def category(request, slug, page=1):
+# 	category = get_object_or_404(Category, slug=slug, status=True)
+# 	articles_list = category.articles.published()
+# 	pagiator = Paginator(articles_list, 1)
+# 	articles = pagiator.get_page(page)
+# 	context = {
+# 		"articles": articles,
+# 		"category": category
+# 	}
 
-	return render(request, 'blog/category.html', context)
+# 	return render(request, 'blog/category.html', context)
+
+
+class CategoryList(ListView):
+	paginate_by = 1
+	template_name = "blog/category_list.html"
+
+	def get_queryset(self):
+		global category
+		slug = self.kwargs.get('slug')
+		category = get_object_or_404(Category.objects.published(), slug=slug,)
+		return category.articles.published()
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['category'] = category
+		return context
