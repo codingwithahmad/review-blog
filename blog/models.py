09ec4b0f653002e70a71c386b6592ecc6ab64922
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-10-08 18:18:14 +0330

    commit-49

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from account.models import User
 from django.utils.html import format_html
 from django.utils import timezone
 from extension.utils import jalali_convertor
