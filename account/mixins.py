717cbfb8831600449678407fc217835304cf062b
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-10-24 20:44:55 +0330

    commit-65

--- account/mixins.py
+++ account/mixins.py
@@ -4,16 +4,12 @@ from blog.models import Article
 
 class FieldMixin():
 	def dispatch(self, request, *args, **kwargs):
+		self.fields = [ 
+			"title", "slug", "category", "description",
+		 	"thumbnail", "publish", "is_special", "status",
+		 ]
 		if request.user.is_superuser:
-			self.fields = [ "author", "title", "slug", "category", "description",
-			 "thumbnail", "publish", "is_special", "status",
-			 ]
-		elif request.user.is_author:
-			self.fields = [ "title", "slug", "category", "description",
-			 "thumbnail", "publish", "is_special",
-			 ]
-		else:
-			raise Http404("شما حق دسترسی به این صفحه را ندارید")
+			self.fields.append("author") 
 
 		return super().dispatch(request, *args, **kwargs)
 
@@ -25,7 +21,8 @@ class FormValidMixin():
 		else:
 			self.obj = form.save(commit=False)
 			self.obj.author = self.request.user
-			self.obj.status = 'd'
+			if not self.obj.status == 'i':
+				self.obj.status = 'd'
 
 		return super().form_valid(form)
 
@@ -47,7 +44,10 @@ class SuperUserAccessMixin():
 
 class AuthorsAccessMixin():
 	def dispatch(self, request, *args, **kwargs):
-		if request.user.is_superuser or request.user.is_author:
-			return super().dispatch(request, *args, **kwargs)
+		if request.user.is_authenticated:
+			if request.user.is_superuser or request.user.is_author:
+				return super().dispatch(request, *args, **kwargs)
+			else:
+				return redirect("account:profile")
 		else:
-			return redirect("account:profile")
+			return redirect("account:login")
