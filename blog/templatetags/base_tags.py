d956ac5aa536a4b196083e78b3dff54a3533b2db
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-11-16 21:40:41 +0330

    commit-79

--- blog/templatetags/base_tags.py
+++ blog/templatetags/base_tags.py
@@ -2,6 +2,7 @@ from django import template
 from ..models import Category, Article
 from datetime import datetime, timedelta
 from django.db.models import Count, Q
+from django.contrib.contenttypes.models import ContentType
 
 register = template.Library()
 
@@ -17,11 +18,21 @@ def category_navbar():
 		"categories": Category.objects.filter(status=True),
 	}
 
-@register.inclusion_tag("blog/partials/popular_articles.html")
+@register.inclusion_tag("blog/partials/sidebar.html")
 def popular_articles():
 	last_month = datetime.today() - timedelta(days=30)
 	return {
-		"popular_articles": Article.objects.published().annotate(count=Count('hits', filter=Q(articlehits__created__gte=last_month))).order_by('-count', '-publish')[:5],
+		"articles": Article.objects.published().annotate(count=Count('hits', filter=Q(articlehits__created__gte=last_month))).order_by('-count', '-publish')[:5],
+		"title": "مقالالت پر بازدید ماه"
+	}
+
+@register.inclusion_tag("blog/partials/sidebar.html")
+def hot_articles():
+	last_month = datetime.today() - timedelta(days=30)
+	content_type_id = ContentType.objects.get(app_label='blog', model='article').id
+	return {
+		"articles": Article.objects.published().annotate(count=Count('comments', filter=Q(comments__posted__gte=last_month) and Q(comments__content_type_id=content_type_id))).order_by('-count', '-publish')[:5],
+		"title": "مقالات داغ ماه"
 	}
 
 
